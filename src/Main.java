427f150f071ed635a69cd9b02327c118d5d715d8
Leting Cai <letingc@nycstudents.net>
2022-12-06 12:27:27 -0500

    1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args){
-
+        System.out.println("                Multiplication Table\n       1   2   3   4   5   6   7   8   9  10  11  12\n----------------------------------------------------");
+        for(int x = 1; x<=12; x++){
+            if(x>9){
+                System.out.print(x+" |");
+            } else {
+                System.out.print(" "+x+" |");
+            }
+            for(int y = 1; y<=12;y++){
+                if(x*y>99){
+                    System.out.print(" ");
+                } else if(x*y>9){
+                    System.out.print("  ");
+                } else {
+                    System.out.print("   ");
+                }
+                System.out.print(x*y);
+            }
+            System.out.println();
+        }
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,44 @@
 public class Person {
+    private String firstName, lastName;
+    private int meeting;
+    public Person(String firstName, String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        meeting = 0;
+    }
 
+    public void meet(Person person2){
+        meeting++;
+        person2.meeting++;
+        System.out.println("Hello my name is "+firstName+"\nHi, my name is "+person2.firstName);
+    }
+
+    public int getMeetings(){
+        return meeting;
+    }
+public static void main(String[] args){
+    Person person1 = new Person("Adam", "Jones");
+    Person person2 = new Person("John", "Smith");
+
+    person1.meet(person2);
+
+    System.out.println(person1.getMeetings());
+    System.out.println(person2.getMeetings());
+
+
+
+    person2.meet(person1);
+
+    System.out.println(person1.getMeetings());
+    System.out.println(person2.getMeetings());
+
+
+
+    Person person3 = new Person("Amy", "Jordan");
+    person3.meet(person2);
+
+    System.out.println(person1.getMeetings());
+    System.out.println(person2.getMeetings());
+    System.out.println(person3.getMeetings());
+}
 }
